a8fa8be50bf56d7ccdc572cf782dffdf2ddbcf03
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-03 21:06:27 -0500

    added logo and list of facts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import logo from "./img/react-logo.png"
 
-const navbar = (
-    <nav>
-        <h1>Starfruit Cafe</h1>
+const page = (
+    <div>
+        <img src={logo} width="40px" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
-ReactDOM.render(navbar, document.getElementById("root"))
+ReactDOM.render(page, document.getElementById("root"))
