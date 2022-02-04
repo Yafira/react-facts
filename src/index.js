405be6d16cc09d7a1e678157372bf8042a5f85d7
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-03 21:27:09 -0500

    created a component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from "react"
 import ReactDOM from "react-dom"
 import logo from "./img/react-logo.png"
 
-const page = (
-    <div>
-        <img src={logo} width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+function Page() {
+    return (
+      <div>
+          <img src={logo} width="40px" />
+          <h1>Fun facts about React</h1>
+          <ul>
+              <li>Was first released in 2013</li>
+              <li>Was originally created by Jordan Walke</li>
+              <li>Has well over 100K stars on GitHub</li>
+              <li>Is maintained by Facebook</li>
+              <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+      </div>
+    )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
