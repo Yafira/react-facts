2a364a845c9c3b78adad22aaddce29f2864cb90d
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-05 00:26:16 -0500

    implemented style to the react info site

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import logo from "./img/react-logo.png"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function Page() {
-    return (
-      <div>
-          <img src={logo} width="40px" />
-          <h1>Fun facts about React</h1>
-          <ul>
-              <li>Was first released in 2013</li>
-              <li>Was originally created by Jordan Walke</li>
-              <li>Has well over 100K stars on GitHub</li>
-              <li>Is maintained by Facebook</li>
-              <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
-      </div>
-    )
-}
-
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'));
